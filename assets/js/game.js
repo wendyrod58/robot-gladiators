80a98d7be4d77c5079c9ece2be7806466afd635f
Wendy Rodriguez <wendyrod58@gmail.com>
2022-01-10 18:45:47 -0800

    adding end of the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,30 +1,41 @@
-// var playerName = window.prompt("What is your robot's name?");
-
-function fight() {
-    window.alert("The fight has begun!");
-}
-//fight();
-
-// var playerName = window.prompt("What is your robot's name?");
-// window.alert(playerName);
-
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 5;
+var enemyAttack = 12;
 
 
-var playerName = "Tony the Robot";
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+};
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      );
 
-// Tony the Robot is ready for battle!
-console.log("Tony the Robot" + " is ready for battle!");
+   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      );
+   
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
 
-// "Tony the Robot is ready for battle!"
-console.log(playerName + " is ready for battle!");
+   
+    fight();
 
-// "Your robot, Tony the Robot, has won!
-console.log("Your robot, " + playerName + ", has won!");
