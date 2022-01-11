de1640ab480bdf05778455d32db368f60045fc1c
Wendy Rodriguez <wendyrod58@gmail.com>
2022-01-10 17:46:59 -0800

    added console examples

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,30 @@
-window.alert("This is an alert! JavaScript is running!");
-// this creates a function named "fight"
+// var playerName = window.prompt("What is your robot's name?");
 
 function fight() {
     window.alert("The fight has begun!");
-  }
+}
+//fight();
+
+// var playerName = window.prompt("What is your robot's name?");
+// window.alert(playerName);
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
