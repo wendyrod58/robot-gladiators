b29d4f895a2e2bf7e7b29b4c8d25d86e32e2ffb6
Wendy Rodriguez <wendyrod58@gmail.com>
2022-01-10 18:48:56 -0800

    adding conditional

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,13 +21,7 @@ var fight = function() {
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
       );
 
-   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth = playerHealth - enemyAttack;
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-      );
-   
+
   // check enemy's health
   if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
@@ -35,6 +29,24 @@ var fight = function() {
   else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
+
+
+
+   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      );
+
+      // check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+   
 
    
     fight();
